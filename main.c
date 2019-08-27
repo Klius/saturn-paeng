4ff675a901a14caba478bfba65e602e68174853c
Klius <Kliusescarlopini@gmail.com>
2019-08-27 22:34:56 +0200

    added audio, states and lose scenario

--- main.c
+++ main.c
@@ -28,15 +28,16 @@
 
 #include <jo/jo.h>
 static jo_font      *my_font;
+static jo_sound     chime;
 int p1x = 30;
-int p1y = 0;
+int p1y = JO_TV_HEIGHT/2-31;
 int p1w = 8;
 int p1h = 62;
 int p1vel = 2;
 int p1Sprite = 0;
 int p1hit = 0;
 int p2x = JO_TV_WIDTH - 38;
-int p2y = 0;
+int p2y = JO_TV_HEIGHT/2-31;
 int p2w = 8;
 int p2h = 62;
 int p2vel = 2;
@@ -51,19 +52,9 @@ float bvelx = 3;
 float bvely = 0;
 int score[] = {0,0};
 int scoreLimit = 10;
-
-void            draw_circle_at_cursor_pos( int radius)
-{
-    int         angle;
-    while (radius >0 ){
-    for (angle = 0; angle < 360; ++angle){
-        jo_put_pixel_in_background(p1x + jo_cos_mult(radius, angle),
-                                   p1y + jo_sin_mult(radius, angle),
-                                   JO_COLOR_Blue);
-	}
-	 --radius;
-	}
-}
+short winner = 0;
+enum STATE {MAIN, GAME, OPTIONS};
+enum STATE currentState = GAME;
 void 			ball_move(void){
 	bx += bvelx; 
 	by += bvely;
@@ -71,10 +62,16 @@ void 			ball_move(void){
 		bx = JO_TV_WIDTH/2 -8;
 		bvelx *= -1;
 		++score[0];
+		if (score[0] == scoreLimit){
+			winner = 1;
+		}
 	}else if( bx+bw < 0 ){
 		bx = JO_TV_WIDTH/2 -8;
 		bvelx *= -1;
 		++score[1];
+		if (score[1] == scoreLimit){
+			winner = 2;
+		}
 	}
 	if (by < 0){
 		bvely *=-1;
@@ -96,36 +93,58 @@ int			ball_collision(int px, int py,int pw, int ph){
 		bvely = bs*-jo_sinf(bounceAngle);
 		bx += bvelx;
 		by += bvely;
+		jo_audio_play_sound(&chime);
 		return 10;
 		//self:setSpeed(self.speed*-1,self.speed*-math.sin(bounceAngle))
 
 	}
 	return 0;
 }
-void			my_draw(void)
-{
-	jo_font_printf(my_font, JO_TV_HEIGHT/2-40, 20, 4.0f, "%d",score[0]);
-	jo_font_printf(my_font, JO_TV_HEIGHT/2+60, 20, 4.0f, "%d",score[1]);
-	jo_printf(0,1,"bx: %d by:%d",bx);
-	jo_printf(0,2,"p1hit: %d",p1hit);
-	jo_printf(0,3, "p2hit:%d",p2hit);
-	if(p1hit == 0){
-		p1hit = ball_collision(p1x,p1y,p1w,p1h);
-	}else{
-		--p1hit;
-	}if(p2hit == 0 ){
-		p2hit = ball_collision(p2x,p2y,p2w,p2h);
-	}else{
-		--p2hit;
+void			game_draw(void){
+	jo_font_printf(my_font, JO_TV_WIDTH/2-80, 20, 4.0f, "%d",score[0]);
+	jo_font_printf(my_font, JO_TV_WIDTH/2+60, 20, 4.0f, "%d",score[1]);
+	if (winner == 0){
+		if(p1hit == 0){
+			p1hit = ball_collision(p1x,p1y,p1w,p1h);
+		}else{
+			--p1hit;
+		}if(p2hit == 0 ){
+			p2hit = ball_collision(p2x,p2y,p2w,p2h);
+		}else{
+			--p2hit;
+		}
+		ball_move();
 	}
-	ball_move();
+	else{
+		jo_font_printf(my_font, 30, JO_TV_HEIGHT/2-20, 2.0f, "PLAYER %d WINS",winner);
+	}
 	jo_sprite_draw3D2(p1Sprite, p1x, p1y, 500);
 	jo_sprite_draw3D2(p2Sprite, p2x, p2y, 500);
 	jo_sprite_draw3D2(ballSprite,bx,by,500);
 	
-	jo_printf(0, 5, "Dynamic memory usage: %d%%  ", jo_memory_usage_percent());
 }
-
+void			my_draw(void)
+{
+	if (currentState == GAME){
+		game_draw();
+	}
+	jo_printf(0,0,"bx: %d by:%d",bx);
+	jo_printf(0,1,"p1hit: %d",p1hit);
+	jo_printf(0,2, "p2hit:%d",p2hit);
+	jo_printf(0, 3, "Sprite memory usage: %d%% ", jo_sprite_usage_percent()); 
+	jo_printf(0, 4, "Dynamic memory usage: %d%%  ", jo_memory_usage_percent());
+}
+void reset(void){
+	bs = 3;
+	bvelx = 3;
+	bvely = 0;
+	score[0] = 0,score[1]=0;
+	winner = 0;
+	bx = JO_TV_WIDTH/2 -8;
+	by = JO_TV_HEIGHT/2 -8;
+	p1y = JO_TV_HEIGHT/2-31;
+	p2y = JO_TV_HEIGHT/2-31;
+}
 void			my_gamepad(void)
 {
 	if (!jo_is_pad1_available())
@@ -134,13 +153,13 @@ void			my_gamepad(void)
 		p1y -= p1vel;
 	if (jo_is_pad1_key_pressed(JO_KEY_DOWN) && p1y+p1h+p1vel < JO_TV_HEIGHT)
 		p1y += p1vel;
-    /*if (jo_is_pad1_key_pressed(JO_KEY_A)){
-        ++bs;++bvelx;}
-		jo_put_pixel_in_background(p1x, p1y, JO_COLOR_Red);
+	if (jo_is_pad1_key_pressed(JO_KEY_START) && (winner > 0 ) )
+        reset();
+    if (jo_is_pad1_key_pressed(JO_KEY_A))
+        jo_audio_play_cd_track(2, 2, 1);/*
     if (jo_is_pad1_key_down(JO_KEY_B))
         draw_circle_at_cursor_pos(40);
-    if (jo_is_pad1_key_pressed(JO_KEY_START))
-        jo_clear_background(JO_COLOR_White);
+    
 	*/
 	//Player2
 	if(!jo_is_pad2_available())
@@ -149,13 +168,16 @@ void			my_gamepad(void)
 		p2y -= p2vel;
 	if (jo_is_pad2_key_pressed(JO_KEY_DOWN) && p2y+p2h+p2vel < JO_TV_HEIGHT)
 		p2y += p2vel;
-    /*if (jo_is_pad2_key_pressed(JO_KEY_A))
+    //if (jo_is_pad2_key_pressed(JO_KEY_A))
         //jo_put_pixel_in_background(p2x, p2y, JO_COLOR_Red);
-    if (jo_is_pad2_key_down(JO_KEY_B))
+	/*if (jo_is_pad2_key_down(JO_KEY_B))
         //draw_circle_at_cursor_pos(40);
     if (jo_is_pad2_key_pressed(JO_KEY_START))
         //jo_clear_background(JO_COLOR_White);
 	*/
+}
+void			load_audio(void){
+	jo_audio_load_pcm("CHIME.PCM",JoSoundMono16Bit, &chime);
 }
 
 void			jo_main(void)
@@ -170,6 +192,7 @@ void			jo_main(void)
     jo_tga_loader(&bg, "BG", "BAK.TGA", JO_COLOR_Transparent);
     jo_set_background_sprite(&bg, 0, 0);
     jo_free_img(&bg);
+	load_audio();
 	
 	my_font = jo_font_load(JO_ROOT_DIR, "FONT.TGA", JO_COLOR_Green, 8, 8, 2, "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ!\"?=%&',.()*+-/");
 	jo_core_add_callback(my_draw);
